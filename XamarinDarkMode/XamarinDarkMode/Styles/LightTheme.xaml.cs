14e4d218ee18d6968743d070af6e333b4a6790d7
Jorge DevsDNA <jorge@devsdna.com>
2020-02-22 20:14:27 +0100

    Included lost code

---
+++ XamarinDarkMode/XamarinDarkMode/Styles/LightTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace XamarinDarkMode.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LightTheme 
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
